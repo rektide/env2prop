22a088a251b0c275b3cd92817414b94cce52ac57
rektide <rektide@voodoowarez.com>
2016-04-01 03:22:48 -0400

    left pad

--- run.js
+++ run.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 "use strict"
 
-var env2prop= require("./env2prop")
+var env2prop= require( "./env2prop")
 
-function run(opts){
+function run( opts){
 	var
 	  envs= opts&& opts.envs|| process.env,
-	  e2p= env2prop({prefixes: opts&& opts.prefixes})
-	return function mapTo(o){
+	  e2p= env2prop({ prefixes: opts&& opts.prefixes})
+	return function mapTo( o){
 		o= o|| {}
 		for(var i in envs){
 			var
-			  p= e2p(i, opts),
+			  p= e2p( i, opts),
 			  key= p,
-			  val= envs[i],
-			  prefix= key[1],
+			  val= envs[ i],
+			  prefix= key[ 1],
 			  target= o
 			if(prefix){
-				target= o[prefix]|| (o[prefix]= {})
-				key= key[0]
+				target= o[ prefix]|| (o[ prefix]= {})
+				key= key[ 0]
 			}
-			target[key]= val
+			target[ key]= val
 		}
 		return o
 	}
@@ -28,20 +28,18 @@ function run(opts){
 
 function main(){
 	var
-	  prefixes= process.argv.splice(2),
-	  r= module.exports.run({prefixes})(),
-	  json= JSON.stringify(r)
-	console.log(json)
+	  prefixes= process.argv.splice( 2),
+	  r= module.exports.run({ prefixes})(),
+	  json= JSON.stringify( r)
+	console.log( json)
 }
 
-module.exports= function run(opts){
-	return module.exports.run(opts)
+module.exports= function run( opts){
+	return module.exports.run( opts)
 }
 module.exports.run= run
 module.exports.main= main
 
-if(require.main=== module){
+if( require.main=== module){
 	module.exports.main()
 }
-
-
